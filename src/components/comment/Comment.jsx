12c3ce4ad5f70f8c5de4237e2b246ed7277e5d28
Harpreet Singh Chahal <harpreet.chahal1996@gmail.com>
2022-06-01 13:43:23 -0600

    Comment component created differently

--- src/components/comment/Comment.jsx
+++ src/components/comment/Comment.jsx
@@ -5,8 +5,14 @@ import "./comment.css"
 export default function Comment()
 {
     return(
-        <div className="comment">
-            
-        </div>
+        <>
+        <div className="viewComments">
+        <span>View All Comments</span>
+    </div>
+    <div className="profileComment">
+<img className="profilePommentProfileImg" src="/assets/person/1.jpg" alt=""/>
+<span className="postCommentText">My first comment..</span>
+    </div>
+    </>
     )
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,7 +3,6 @@ import Topbar from "../../components/topbar/Topbar";
 import Leftbar from "../../components/leftbar/Leftbar";
 import Feed from "../../components/feed/Feed";
 import Rightbar from "../../components/rightbar/Rightbar";
-import Comment from "../../components/comment/Comment";
 import "./home.css";
 
 
