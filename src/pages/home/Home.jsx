f65c0ae11c5f4cf882e8b25606f73ee3da1149e2
Harpreet Singh Chahal <harpreet.chahal1996@gmail.com>
2022-06-01 14:35:21 -0600

    Profile component created

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,6 +3,7 @@ import Topbar from "../../components/topbar/Topbar";
 import Leftbar from "../../components/leftbar/Leftbar";
 import Feed from "../../components/feed/Feed";
 import Rightbar from "../../components/rightbar/Rightbar";
+import Profile from "../../components/profile/Profile";
 import "./home.css";
 
 
