584991176373079946eaf715f356cee7e87cdc22
Harpreet Singh Chahal <harpreet.chahal1996@gmail.com>
2022-06-02 10:31:16 -0600

    Leftbar MUI card component added

--- src/components/leftbar/Leftbar.jsx
+++ src/components/leftbar/Leftbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./leftbar.css";
 
 
-import{ Home, Person } from "@mui/icons-material";
+import { Home, FavoriteBorder, Person, PersonAdd } from "@mui/icons-material";
 
 import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
@@ -11,6 +11,26 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 import Slide from '@mui/material/Slide';
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Divider from '@mui/material/Divider';
+import HomeIcon from '@mui/icons-material/Home';
+import PersonIcon from '@mui/icons-material/Person';
+import DraftsIcon from '@mui/icons-material/Drafts';
+
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import FolderIcon from '@mui/icons-material/Folder';
+import RestoreIcon from '@mui/icons-material/Restore';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+
+import { shadows } from '@mui/system';
+//import { Box, ThemeProvider, createTheme } from '@mui/system';
 
 const Transition = React.forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
@@ -18,60 +38,62 @@ const Transition = React.forwardRef(function Transition(props, ref) {
 
 
 
-export default function Leftbar()
-{
-    const [open, setOpen] = React.useState(false);
 
-  const handleClickOpen = () => {
-    setOpen(true);
+export default function Leftbar() {
+  const [value, setValue] = React.useState('recents');
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
   };
+  return (
+    <div className="leftbar">
 
-  const handleClose = () => {
-    setOpen(false);
-  };
+      {/* <Box variant="contained" sx={{ border: 1, borderRadius: 2,
+          width: '100%', 
+          maxWidth: 360, 
+          bgcolor: 'background.paper',
+          mt:3,
+          ml:2, 
+          position:'fixed'}}> */}
+      <Box
+        sx={{
+          border: 2,
+          borderRadius: 2,
+          boxShadow: 3,
+          borderColor: 'background.paper',
+          mt: 2.5, ml: 1,
+          width: '100%',
+          maxWidth: 360,
+          bgcolor: 'background.paper',
+          position: 'fixed',
+        }}>
+        <nav aria-label="main list">
+          <List>
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <HomeIcon color="primary" />
+                </ListItemIcon>
+                <ListItemText primary="Home" />
+              </ListItemButton>
+            </ListItem>
+            <Divider />
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <PersonIcon color="primary" />
+                </ListItemIcon>
+                <ListItemText primary="Profile" />
+              </ListItemButton>
+            </ListItem>
+          </List>
+        </nav>
 
-    return(
-        <div className="leftbar">
-            <div className="leftbarWrapper">
-                <ul className="leftbarList">
-                    <li className="leftbarListItem">
-                        <Home className="leftbarIcon" cursor="pointer"/>
-                        <span className="leftbarListItemText">Feed</span>
-                    </li>
-                    <li className="leftbarListItem">
-                        <Person className="leftbarIcon" cursor="pointer"/>
-                        <span className="leftbarListItemText">Profile</span>
-                    </li>
-                </ul>
-            </div>
-            {/* <button className="leftbarLogoutButton">Logout</button> */}
-            <div className="leftbarLogoutButton">
 
-            <Button variant="contained" onClick={handleClickOpen}>
-        Logout
-      </Button>
-      <Dialog
-        open={open}
-        TransitionComponent={Transition}
-        keepMounted
-        onClose={handleClose}
-        aria-describedby="alert-dialog-slide-description"
-      >
-        <DialogTitle>{"Are you sure you want to logout?"}</DialogTitle>
-        {/* <DialogContent>
-          <DialogContentText id="alert-dialog-slide-description">
-            You will have to login again
-          </DialogContentText>
-        </DialogContent> */}
-        <DialogActions>
-          <Button onClick={handleClose}>Cancel</Button>
-          <Button onClick={handleClose}>Log out</Button>
-        </DialogActions>
-      </Dialog>
-            
-            
-            
-            </div>
-        </div>
-    )
+      </Box>
+
+
+    </div>
+
+  )
 }
--- src/components/share/Share.jsx
+++ src/components/share/Share.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 import {UploadFile } from "@mui/icons-material";
-import Button from '@mui/material/Button';
+// import Button from '@mui/material/Button';
 import "./share.css"
+import { styled } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import PhotoCamera from '@mui/icons-material/PhotoCamera';
+import Stack from '@mui/material/Stack';
+
+const Input = styled('input')({
+  display: 'none',
+});
 
 
 export default function Share() {
@@ -15,9 +24,23 @@ export default function Share() {
                 <hr className="shareHr" />
                 <div className="shareBottom">
                     <div className="shareOption">
-                    <Button variant="contained" color="success" startIcon={<UploadFile />}>
+                    {/* <Button variant="contained" color="success" startIcon={<UploadFile />}>
                        Upload Image
-                    </Button>
+                    </Button> */}
+                       <Stack direction="row" alignItems="center" spacing={2}>
+      <label htmlFor="contained-button-file">
+        <Input accept="image/*" id="contained-button-file" multiple type="file" />
+        <Button variant="contained" component="span">
+          Upload
+        </Button>
+      </label>
+      <label htmlFor="icon-button-file">
+        <Input accept="image/*" id="icon-button-file" type="file" />
+        <IconButton color="primary" aria-label="upload picture" component="span">
+          <PhotoCamera />
+        </IconButton>
+      </label>
+    </Stack>
                     </div>
                     <Button variant="contained" >
                         Create Post
