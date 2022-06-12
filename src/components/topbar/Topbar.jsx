e3f7ad42e11cdc811bb6af2688e35e6b5114431f
Harpreet Singh Chahal <harpreet.chahal1996@gmail.com>
2022-06-11 18:43:30 -0600

    Error while changing logout to topbar (commented)

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -4,15 +4,33 @@
  * @date      02-06-2022
  * @brief     This is the topbar component page (navigation bar) for LookMeUp project.
  */
-
+import React from "react";
 import "./topbar.css";
 import { useContext } from "react";
 import { Search } from "@mui/icons-material";
 import { Context } from "../../components/context/Context";
 import { useNavigate } from "react-router-dom";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import Settings from '@mui/icons-material/Settings';
+import Logout from '@mui/icons-material/Logout';
+
+
 export default function Topbar({ fetchPosts }) {
   const { user } = useContext(Context);
   const navigate = useNavigate();
+
+  // const [anchorEl, setAnchorEl] = React.useState(null);
+  // const open = Boolean(anchorEl);
+  // const handleClick = (event) => {
+  //   setAnchorEl(event.currentTarget);
+  // };
+  // const handleClose = () => {
+  //   setAnchorEl(null);
+  // };
+
+
   return (
     <div className="topbarContainer">
       <div className="topbarLeft">
@@ -46,7 +64,33 @@ export default function Topbar({ fetchPosts }) {
           onClick={() => {
             navigate("/profile");
           }}
-        />
+        >
+
+{/* <Menu
+        anchorEl={anchorEl}
+        id="account-menu"
+        open={open}
+        onClose={handleClose}
+        onClick={handleClose}
+        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+      >
+       
+        <MenuItem>
+          <ListItemIcon>
+            <Settings fontSize="small" />
+          </ListItemIcon>
+          Settings
+        </MenuItem>
+        <MenuItem>
+          <ListItemIcon>
+            <Logout fontSize="small" />
+          </ListItemIcon>
+          Logout
+        </MenuItem>
+      </Menu> */}
+
+          </img>
         {/* <div className="logout">
                     <Button variant="contained" color="success" endIcon={<UploadFile />}>
                        Logout
